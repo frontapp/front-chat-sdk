fc3c7391f0120e350d45b6ef0d5d2d6659687edd
Gregory Linscheid <linscheid.greg@gmail.com>
2023-12-09 14:49:15 -0800

    :shirt: No eslint at all for example files

--- examples/react-initialize-and-show/index.tsx
+++ examples/react-initialize-and-show/index.tsx
@@ -12,7 +12,6 @@ const chatId = '<CHAT_ID_REQUIRED>';
  * Component.
  */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, react-refresh/only-export-components
 function App() {
   const {frontChat, initialize, isInitialized} = useFrontChat(document.body);
 
